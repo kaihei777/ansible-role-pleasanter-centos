1b0423218cce15c1be8e554b6c9bd258af77f6ef
Kaihei Sameshima <sameshima@asd-inc.jp>
2020-04-08 16:13:24 +0900

    rc

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ export ANSIBLE_CONFIG=tests/ansible.cfg
 
 ansible-galaxy install -r tests/requirements.yml --force
 
-ansible-playbook -i tests/inventory tests/test.yml
+ansible-playbook -i tests/inventory tests/test.yml -vv
